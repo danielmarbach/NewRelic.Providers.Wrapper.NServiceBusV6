c986e4124c3a2adcf6e01f00dfab92e09c8b35e6
danielmarbach <daniel.marbach@openplace.net>
2017-04-07 15:17:12 +0200

    Copy Dependencies script

---
+++ CopyNewRelicDependencies.cmd
@@ -0,0 +1,3 @@
+mkdir .\libs
+copy "%ProgramFiles%\New Relic\.NET Agent\NewRelic.Agent.Extensions.dll" .\libs\NewRelic.Agent.Extensions.dll
+copy "%ProgramFiles%\New Relic\.NET Agent\Extensions\NewRelic.SystemExtensions.dll" .\libs\NewRelic.SystemExtensions.dll
