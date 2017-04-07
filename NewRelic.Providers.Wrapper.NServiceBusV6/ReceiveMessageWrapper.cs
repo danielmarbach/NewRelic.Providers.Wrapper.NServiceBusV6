c0fa4e35628f7568aa73b50cdfb36890089b24ee
danielmarbach <daniel.marbach@openplace.net>
2017-04-07 12:09:32 +0200

    NewRelic spike

---
+++ NewRelic.Providers.Wrapper.NServiceBusV6/ReceiveMessageWrapper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using NewRelic.Agent.Extensions.Providers.Wrapper;
+using NewRelic.SystemExtensions;
+using NServiceBus.Pipeline;
+
+namespace NewRelic.Providers.Wrapper.NServiceBusV6
+{
+    public class ReceiveMessageWrapper : IWrapper
+    {
+
+        public CanWrapResponse CanWrap(InstrumentedMethodInfo methodInfo)
+        {
+            return new CanWrapResponse(
+                MethodExtensions.MatchesAny((Method) methodInfo.Method, "NServiceBus.Core",
+                    "NServiceBus.LoadHandlersConnector", "Invoke",
+                    "NServiceBus.Pipeline.IIncomingLogicalMessageContext,System.Func`2[NServiceBus.Pipeline.IInvokeHandlerContext,System.Threading.Tasks.Task]"),
+                (string) null);
+        }
+
+        public AfterWrappedMethodDelegate BeforeWrappedMethod(InstrumentedMethodCall instrumentedMethodCall,
+            IAgentWrapperApi agentWrapperApi)
+        {
+            var context = instrumentedMethodCall.MethodCall.MethodArguments
+                .ExtractNotNullAs<IIncomingLogicalMessageContext>(0);
+            var incomingLogicalMessage = context.Message;
+            if (incomingLogicalMessage == null)
+                throw new NullReferenceException("logicalMessage");
+            Dictionary<string, string> headers = context.Headers;
+            if (headers == null)
+                throw new NullReferenceException("headers");
+            string queueName = TryGetQueueName(incomingLogicalMessage);
+            agentWrapperApi.CreateMessageBrokerTransaction(0, "NServiceBus", queueName);
+            ISegment isegment = agentWrapperApi.StartMessageBrokerSegment(instrumentedMethodCall.MethodCall, 0,
+                (MessageBrokerAction) 1, "NServiceBus", queueName);
+            agentWrapperApi.ProcessInboundRequest(headers,
+                new int?());
+
+            return Delegates.GetDelegateFor(() =>
+            {
+                agentWrapperApi.EndSegment(isegment);
+                agentWrapperApi.EndTransaction();
+            }, null, agentWrapperApi.NoticeError);
+        }
+
+        private static string TryGetQueueName(LogicalMessage logicalMessage)
+        {
+            return logicalMessage.MessageType.FullName;
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,9 @@
+Create a lib folder that contains
+
+NewRelic.Agent.Extensions.dll
+NewRelic.SystemExtensions.dll
+
+Compile Release build
+
+Drop assembly into Agent extensions directory
+Drop instrumentation.xml into extensions directory under ProgramData
